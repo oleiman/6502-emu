95056d653307eccaa4078e250e8f6ca32c617699
Oren Leiman <oren.leiman@protonmail.com>
2020-12-01 15:06:17 -0800

    Bit of test work

--- test/inc/util.hpp
+++ test/inc/util.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <sstream>
 #include <string>
 
-const std::string AllSuitePath = "rom/AllSuiteA.bin";
-const std::string KlausFunctionalPath = "rom/6502_functional_test.bin";
-const std::string KlausDecimalPath = "rom/6502_decimal_test.bin";
+const std::string AllSuiteA = "rom/AllSuiteA.bin";
+const std::string KlausFunctional = "rom/6502_functional_test.bin";
+const std::string BruceClarkDecimal = "rom/6502_decimal_test.bin";
+const std::string KlausInterrupt = "rom/6502_interrupt_test.bin";
+
+std::stringstream report(std::string const &rom, int cycles, int instructions);
--- test/src/cpu.cpp
+++ test/src/cpu.cpp
@@ -11,8 +11,8 @@
 #include <iostream>
 #include <memory>
 
-#define xstr(s) str(s)
-#define str(s) #s
+#define xestr(s) estr(s)
+#define estr(s) #s
 
 using std::make_shared;
 using std::filesystem::current_path;
@@ -25,39 +25,40 @@ constexpr int memory_size = 0x10000;
 TEST_CASE("AllSuiteA", "[integration][cpu]") {
   Ram<memory_size, M6502::AddressT, M6502::DataT> memory;
   int cycles = 0;
+  int instructions = 0;
   auto tick = [&cycles]() { ++cycles; };
 
   M6502 cpu(memory.addressBus(), memory.dataBus(), tick);
 
   // 0x4000 specific to test program from interwebs
-  current_path(xstr(SOURCE_DIR));
-  std::ifstream infile(AllSuitePath, std::ios::binary);
+  current_path(xestr(SOURCE_DIR));
+  std::ifstream infile(AllSuiteA, std::ios::binary);
   REQUIRE(cpu.loadRom(infile, 0x4000));
   infile.close();
   cpu.initPc(0x4000);
 
-  while (true) {
-    if (cpu.pc() == 0x45c0)
-      break;
+  do {
+    ++instructions;
     cpu.step();
-  }
+  } while (cpu.pc() != 0x45c0);
 
   memory.addressBus().put(0x0210);
   REQUIRE(memory.dataBus().get() == 0xff);
 
-  std::cerr << cycles << std::endl;
+  std::cerr << report(AllSuiteA, cycles, instructions).str() << std::endl;
 }
 
 TEST_CASE("KlausFunctional", "[integration][cpu]") {
   Ram<memory_size, M6502::AddressT, M6502::DataT> memory;
   int cycles = 0;
+  int instructions = 0;
   auto tick = [&cycles]() { ++cycles; };
 
   M6502 cpu(memory.addressBus(), memory.dataBus(), tick);
 
   // 0x400 specific to test program from interwebs
-  current_path(xstr(SOURCE_DIR));
-  std::ifstream infile(KlausFunctionalPath, std::ios::binary);
+  current_path(xestr(SOURCE_DIR));
+  std::ifstream infile(KlausFunctional, std::ios::binary);
   REQUIRE(cpu.loadRom(infile, 0x0000u));
   infile.close();
   cpu.initPc(0x400u);
@@ -68,6 +69,7 @@ TEST_CASE("KlausFunctional", "[integration][cpu]") {
   try {
     do {
       // cpu.debugStep(d);
+      ++instructions;
       cpu.step();
     } while (cpu.pc() != 0x3469);
   } catch (std::runtime_error e) {
@@ -77,38 +79,70 @@ TEST_CASE("KlausFunctional", "[integration][cpu]") {
 
   // memory.addressBus().put(0x0210);
   REQUIRE(cpu.pc() == 0x3469);
-  std::cerr << cycles << std::endl;
+  std::cerr << report(KlausFunctional, cycles, instructions).str() << std::endl;
 }
 
-TEST_CASE("KlausDecimal", "[integration][cpu]") {
+TEST_CASE("BruceClarkDecimal", "[integration][cpu]") {
   Ram<memory_size, M6502::AddressT, M6502::DataT> memory;
   int cycles = 0;
+  int instructions = 0;
   auto tick = [&cycles]() { ++cycles; };
 
   M6502 cpu(memory.addressBus(), memory.dataBus(), tick);
 
   // 0x400 specific to test program from interwebs
-  current_path(xstr(SOURCE_DIR));
-  std::ifstream infile(KlausDecimalPath, std::ios::binary);
+  current_path(xestr(SOURCE_DIR));
+  std::ifstream infile(BruceClarkDecimal, std::ios::binary);
   REQUIRE(cpu.loadRom(infile, 0x0200u));
   infile.close();
   cpu.initPc(0x200u);
 
   // dbg::Debugger d(true, false);
 
-  // BENCHMARK("Klaus Functional Tests") {
   try {
     do {
       // cpu.debugStep(d);
+      ++instructions;
       cpu.step();
-    } while (cpu.pc() != 0x024b);
+    } while (cpu.pc() != 0x025b);
   } catch (std::runtime_error e) {
     std::cerr << e.what() << std::endl;
   }
-  // }
 
-  // memory.addressBus().put(0x0210);
-  REQUIRE(cpu.pc() == 0x024b);
-  // REQUIRE(cycles == 3807);
-  std::cerr << cycles << std::endl;
+  REQUIRE(cpu.pc() == 0x025b);
+  // REQUIRE(cycles == 7915081);
+  std::cerr << report(BruceClarkDecimal, cycles, instructions).str()
+            << std::endl;
 }
+
+// TEST_CASE("KlausInterrupt", "[integration][cpu]") {
+//   Ram<memory_size, M6502::AddressT, M6502::DataT> memory;
+//   int cycles = 0;
+//   int instructions = 0;
+//   auto tick = [&cycles]() { ++cycles; };
+
+//   M6502 cpu(memory.addressBus(), memory.dataBus(), tick);
+
+//   // 0x400 specific to test program from interwebs
+//   current_path(xestr(SOURCE_DIR));
+//   std::ifstream infile(KlausInterrupt, std::ios::binary);
+//   REQUIRE(cpu.loadRom(infile, 0x0000u));
+//   infile.close();
+//   cpu.initPc(0x400u);
+
+//   // dbg::Debugger d(true, false);
+
+//   try {
+//     do {
+//       // cpu.debugStep(d);
+//       ++instructions;
+//       cpu.step();
+//     } while (cpu.pc() != 0x025b);
+//   } catch (std::runtime_error e) {
+//     std::cerr << e.what() << std::endl;
+//   }
+
+//   REQUIRE(cpu.pc() == 0x025b);
+//   std::cerr << report(KlausInterrupt, cycles, instructions).str() <<
+//   std::endl;
+// }
